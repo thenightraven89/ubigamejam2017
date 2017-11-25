6884dfa83b0b4ac9d3c0db76da7d49647aa9915f
Bogdan Tănăsoiu <bogdan.tana@gmail.com>
2017-11-25 16:53:29 +0200

    added dash mechanic, with cooldown

--- Assets/_MainAssets/Scripts/Character.cs
+++ Assets/_MainAssets/Scripts/Character.cs
@@ -6,38 +6,71 @@ public class Character : MonoBehaviour
 	private int _playerIndex;
 	private string _xaxisName;
 	private string _yaxisName;
+	private string _dashName;
 	private float _xaxis;
 	private float _yaxis;
-	private float _xspeed = 0.1f;
-	private float _yspeed = 0.1f;
+	private float _dashMultiplier;
 	private Transform _t;
 	private bool _isStunned;
+	private Vector2 _baseSpeed;
+	private bool _dashAvailable;
+
+	private const float DASH_COOLDOWN = 3f;
+	private const float DASH_MULTIPLIER_MAX = 3f;
+	private const float DASH_DECAY = 0.25f; //decay in seconds
+	private const float XSPEED = 0.05f;
+	private const float YSPEED = 0.05f;
 
 	public void Initialize(int playerIndex)
 	{
 		_playerIndex = playerIndex;
 		_xaxisName = string.Format("xaxis_{0}", playerIndex);
 		_yaxisName = string.Format("yaxis_{0}", playerIndex);
+		_dashName = string.Format("dash_{0}", playerIndex);
 		_t = transform;
 		_isStunned = false;
+		_dashMultiplier = 1f;
+		_dashAvailable = true;
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
 		_xaxis = Input.GetAxis(_xaxisName);
 		_yaxis = Input.GetAxis(_yaxisName);
+		_baseSpeed = new Vector2(_xaxis, _yaxis).normalized;
 
 		if (!_isStunned)
 		{
-			_t.Translate(new Vector2(_xaxis * _xspeed, _yaxis * _yspeed));
+			_t.Translate(new Vector2(_baseSpeed.x * XSPEED * _dashMultiplier, _baseSpeed.y * YSPEED * _dashMultiplier));
+
+			if (Input.GetButtonDown(_dashName) && _dashAvailable)
+			{
+				StartCoroutine(DashCoroutine(DASH_DECAY, DASH_COOLDOWN));
+			}
 		}
 	}
 
 	public void Stun(float duration)
 	{
 		StopAllCoroutines();
+		_dashMultiplier = 1f;
+		_dashAvailable = true;
 		StartCoroutine(StunCoroutine(duration));
+	}
+
+	private IEnumerator DashCoroutine(float decay, float cooldown)
+	{
+		_dashAvailable = false;
+		_dashMultiplier = DASH_MULTIPLIER_MAX;
+		float t = 0f;
+		while (t < decay)
+		{
+			_dashMultiplier = Mathf.Lerp(1f, DASH_MULTIPLIER_MAX, 1 - t / decay);
+			yield return null;
+			t += Time.deltaTime;
+		}
+		yield return new WaitForSeconds(cooldown);
+		_dashAvailable = true;
 	}
 
 	private IEnumerator StunCoroutine(float duration)
