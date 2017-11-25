b72ee2acae9146d8bace09a55a5e57d0789343d7
Jude <tudor.jude47@gmail.com>
2017-11-25 10:27:17 +0200

    Doamne'ajuta
    Doamne'ajuta

---
+++ Assets/_MainAssets/Scripts/GameMaster.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
